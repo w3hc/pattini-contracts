8ac8f4df281d8a40567277ac9ef510fe95092c23
julienbrg <julien@strat.cc>
2024-03-28 08:24:59 +0100

    fix test

--- test/Pattini.ts
+++ test/Pattini.ts
@@ -11,9 +11,9 @@ describe("Pattini", function () {
         const initialOwner = alice.address
 
         const repositoryName = "github-action-test"
-        const EUR = await ethers.getContractFactory("EUR")
-        const eur = await EUR.deploy()
-        const tokenAddress = await eur.getAddress()
+        const OP = await ethers.getContractFactory("OP")
+        const op = await OP.deploy()
+        const tokenAddress = await op.getAddress()
 
         const funderAddress = alice.address
         const Pattini = await ethers.getContractFactory("Pattini")
@@ -23,8 +23,8 @@ describe("Pattini", function () {
             tokenAddress,
             funderAddress
         )
-        await eur.transfer(await pattini.getAddress(), ethers.parseEther("200"))
-        await eur.approve(await pattini.getAddress(), ethers.parseEther("200"))
+        await op.transfer(await pattini.getAddress(), ethers.parseEther("200"))
+        await op.approve(await pattini.getAddress(), ethers.parseEther("200"))
 
         return {
             pattini,
@@ -33,16 +33,16 @@ describe("Pattini", function () {
             repositoryName,
             funderAddress,
             tokenAddress,
-            eur
+            op
         }
     }
 
     describe("Deployment", function () {
         it("Should return the right token address", async function () {
-            const { pattini, tokenAddress, eur } = await loadFixture(
+            const { pattini, tokenAddress, op } = await loadFixture(
                 deployContracts
             )
-            expect(await eur.balanceOf(await pattini.getAddress())).to.be.equal(
+            expect(await op.balanceOf(await pattini.getAddress())).to.be.equal(
                 ethers.parseEther("200")
             )
             expect(await pattini.tokenAddress()).to.equal(tokenAddress)
@@ -57,31 +57,31 @@ describe("Pattini", function () {
         })
 
         it("Should pay a contributor", async function () {
-            const { pattini, bob, eur } = await loadFixture(deployContracts)
+            const { pattini, bob, op } = await loadFixture(deployContracts)
 
             await pattini.take(88888, 42, bob.address)
             expect((await pattini.getIssue(88888))[1]).to.be.equal(42)
 
-            expect(await eur.balanceOf(bob.address)).to.be.equal(
+            expect(await op.balanceOf(bob.address)).to.be.equal(
                 ethers.parseEther("0")
             )
             await pattini.pay(88888, 83)
-            expect(await eur.balanceOf(bob.address)).to.be.equal(
+            expect(await op.balanceOf(bob.address)).to.be.equal(
                 ethers.parseEther("42")
             )
         })
 
         it("Should not take the issue", async function () {
-            const { pattini, bob, eur } = await loadFixture(deployContracts)
+            const { pattini, bob, op } = await loadFixture(deployContracts)
 
             await pattini.take(88888, 42, bob.address)
             expect((await pattini.getIssue(88888))[1]).to.be.equal(42)
 
-            expect(await eur.balanceOf(bob.address)).to.be.equal(
+            expect(await op.balanceOf(bob.address)).to.be.equal(
                 ethers.parseEther("0")
             )
             await pattini.pay(88888, 83)
-            expect(await eur.balanceOf(bob.address)).to.be.equal(
+            expect(await op.balanceOf(bob.address)).to.be.equal(
                 ethers.parseEther("42")
             )
 
@@ -93,33 +93,33 @@ describe("Pattini", function () {
         })
 
         it("Should flush", async function () {
-            const { pattini, bob, eur } = await loadFixture(deployContracts)
+            const { pattini, bob, op } = await loadFixture(deployContracts)
 
             await pattini.take(88888, 42, bob.address)
             expect((await pattini.getIssue(88888))[1]).to.be.equal(42)
 
-            expect(await eur.balanceOf(await pattini.getAddress())).to.be.equal(
+            expect(await op.balanceOf(await pattini.getAddress())).to.be.equal(
                 ethers.parseEther("200")
             )
             await pattini.take(55555, 58, bob.address)
 
             await pattini.flush()
 
-            expect(await eur.balanceOf(await pattini.getAddress())).to.be.equal(
+            expect(await op.balanceOf(await pattini.getAddress())).to.be.equal(
                 ethers.parseEther("100")
             )
         })
 
         it("Should handle multiple entry for the same issue", async function () {
-            const { pattini, bob, eur } = await loadFixture(deployContracts)
+            const { pattini, bob, op } = await loadFixture(deployContracts)
             await pattini.take(88888, 10, bob.address)
             expect((await pattini.getIssue(88888))[1]).to.be.equal(10)
-            expect(await eur.balanceOf(await pattini.getAddress())).to.be.equal(
+            expect(await op.balanceOf(await pattini.getAddress())).to.be.equal(
                 ethers.parseEther("200")
             )
             await pattini.take(88888, 10, bob.address)
             await pattini.flush()
-            expect(await eur.balanceOf(await pattini.getAddress())).to.be.equal(
+            expect(await op.balanceOf(await pattini.getAddress())).to.be.equal(
                 ethers.parseEther("200")
             )
         })
